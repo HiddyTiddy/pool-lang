e44b62a7f59bd04d69489338308e40e1c2655072
HiddyTiddy <hiddy.tiddey@gmail.com>
2021-10-30 20:00:05 +0200

    added command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use clap::{Arg, App};
 
 mod pool;
 use pool::interpret;
@@ -8,12 +8,30 @@ mod util;
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() == 1 {
-        eprintln!("missing arguments");
-        return;
+    // let args: Vec<String> = env::args().collect();
+    // if args.len() == 1 {
+    //     eprintln!("missing arguments");
+    //     return;
+    // }
+    // let filename = &args[1];
+    let argument_matches = App::new("pool interpreter")
+                    .version("0.1.0")
+                    .author("hyde <hiddy.tiddey@gmail.com>")
+                    .about("interpreter for the pool 2d language")
+                    .arg(Arg::with_name("filename")
+                        .takes_value(true)
+                        .value_name("FILE")
+                        .help("set filename")
+                        .index(1)
+                    ).get_matches();
+
+        
+    let f = argument_matches.value_of("filename");
+    
+    if let Some(filename) = f {
+        let grid = util::read_file(filename).expect("file not found");
+        interpret(grid);   
+    } else {
+        println!("no file specified");
     }
-    let filename = &args[1];
-    let grid = util::read_file(filename).expect("file not found");
-    interpret(grid);
 }
