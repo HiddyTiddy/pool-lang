5740e32f5b9655f70cadf3627aaaeb95c63e4407
HiddyTiddy <hiddy.tiddey@gmail.com>
2021-10-30 23:13:35 +0200

    add graphical flag

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
+use std::process::exit;
+
 use clap::{Arg, App};
 
 mod pool;
 use pool::interpret;
 
+use crate::graphical::graphical_interpret;
+
+mod graphical;
 mod util;
 
 
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // if args.len() == 1 {
-    //     eprintln!("missing arguments");
-    //     return;
-    // }
-    // let filename = &args[1];
     let argument_matches = App::new("pool interpreter")
                     .version("0.1.0")
                     .author("hyde <hiddy.tiddey@gmail.com>")
@@ -23,14 +22,29 @@ fn main() {
                         .value_name("FILE")
                         .help("set filename")
                         .index(1)
-                    ).get_matches();
+                    )
+                    .arg(Arg::with_name("graphical_mode")
+                        .short("g")
+                        .long("graphical")
+                        .takes_value(false)
+                        .help("run program in graphical mode")
+                    )
+                    .get_matches();
 
         
     let f = argument_matches.value_of("filename");
     
+    let graphical_mode = argument_matches.occurrences_of("graphical_mode") >= 1;
+
     if let Some(filename) = f {
         let grid = util::read_file(filename).expect("file not found");
-        interpret(grid);   
+        if graphical_mode {
+            let exit_code = graphical_interpret(grid);
+            exit((exit_code.or::<i64>(Ok(-1)).expect(".") & 0xffffffff) as i32);
+        } else {
+            let exit_code = interpret(grid);
+            exit((exit_code & 0xffffffff) as i32);
+        }
     } else {
         println!("no file specified");
     }
